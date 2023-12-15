4ef7d58cfaeafe9824d6e4e1d5cbcdb40fbb4fd1
kimchang312 <puckerydaum.312@gmail.com>
2023-12-15 10:08:07 +0900

    수정

--- script/reviews.page.js
+++ script/reviews.page.js
@@ -7,8 +7,21 @@
     }).
     then(data =>{
         data.data.map((e)=>{
+            let reviswsParent=document.getElementById("reviswsParent");
+            let newChildElement = document.createElement("div");
+            reviswsParent.appendChild(newChildElement);
+            newChildElement.innerHTML = `
+            <div class="reviews"> 
+            <div class="sitterName" id="reviewSitterName">${e.sitterName}</div>
+            <div class="reviewOne" >
+            <div class="userName" id="reviewUserName">${e.userName}</div>
+            <div class="reviewComment" id="reviewUserComment">${e.comment}</div>
+            <div class="reviewGrade" id="reviewUserGrade">${"별점:"+e.grade}</div>
+            </div>
+            </div>
+            `;
             /*
-            class reviews의 자식객체 생성 
+            id reviswsParent 자식객체 생성 
             자식객체에 아래 innerHTML로 생성
             <div class="sitterName" id="reviewSitterName">${e.sitterName}</div>
             <div class="reviewOne" >
@@ -17,16 +30,17 @@
             <div class="reviewGrade" id="reviewUserGrade">${"별점:"+e.grade}</div>
             </div>
             */
+           /*
             const sitterName= document.getElementById("reviewSitterName");
             const userName= document.getElementById("reviewUserName");
             const reviewComment= document.getElementById("reviewUserComment");
             const reviewGrade= document.getElementById("reviewUserGrade");
-
+            
             sitterName.innerHTML=e.sitterName;
             userName.innerHTML=e.userName;
             reviewComment.innerHTML=e.comment;
             reviewGrade.innerHTML="별점:"+e.grade;
-    
+    */
         })
        
        
